6f2736b303d8f9a3e6498e608d605ab7a3f81235
Reese Jenkins <85418720+reese-oo@users.noreply.github.com>
2021-10-10 14:47:29 -0500

    js files

---
+++ static/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#ul1").hide();
+    $("#p").hide();
+    $("#num").hide();
+});
+
+$(document).ready(function(){
+  $("#div4").click(function(){
+    $("#ul1").slideToggle();
+    });
+});
+
+$(document).ready(function(){
+   $("#div3").click(function(){
+      $("#p").slideToggle("slow");
+   });
+});
+
+
+alert("Info: View looks best at 125% Zoom and if having Java issues follow the link below.")
+  
+
+function disapear() {
+
+  document.getElementId("s-h1").innerHTML="hgcjgfm"
+  
+};
+
+$(document).ready(function(){
+   $("#num-0").click(function(){
+      $("#num").slideToggle("slow");
+   });
+});
